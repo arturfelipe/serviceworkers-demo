2af16eb614dd450450474cf56e4423510410eb6c
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-01-08 09:05:24 -0200

    Create a simple sw sample

---
+++ some-cool-bar/sw.js
@@ -0,0 +1,48 @@
+// Service Worker Scope V1
+
+const CACHE_VERSION = 'v1';
+
+self.addEventListener('install', event => {
+  console.log(`[SW] Installing ${CACHE_VERSION}`);
+
+  // Precaching
+  event.waitUntil(
+    caches.open(`some-cool-bar-${CACHE_VERSION}`)
+      .then(cache => {
+        console.log(`[SW] Precaching ${CACHE_VERSION}`);
+        return cache.addAll([
+          '/',
+          '/img/bar.svg',
+          '/img/milk.svg'
+        ])
+      })
+  );
+
+});
+
+self.addEventListener('activate', event => {
+  console.log(`[SW] Activated ${CACHE_VERSION}  `);
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Serve a glass of milk instead of a beer
+  if (url.origin == location.origin && url.pathname.endsWith('/img/beer.svg')) {
+    return event.respondWith(caches.match('/img/milk.svg'));
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+
+        // Cache hit - return response from cache
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
